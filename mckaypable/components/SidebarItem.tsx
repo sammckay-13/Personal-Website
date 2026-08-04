19beb969ca8e592fe2978021fadc395ef98b4fe0
Sam McKay <sammckay31@gmail.com>
2026-08-04 12:37:26 -0600

    change icons and add motion divs to bring in text in a visual appealing way. I'm trying to animate the icons too

--- mckaypable/components/SidebarItem.tsx
+++ mckaypable/components/SidebarItem.tsx
@@ -1,47 +1,63 @@
 "use client";
-import Download from "@/assets/imgs/Download.png";
-import email from "@/assets/imgs/email.png";
-import github from "@/assets/imgs/github.png";
-import Home_icon from "@/assets/imgs/Home_Icon.png";
-import LinkedIn from "@/assets/imgs/LinkedIn.png";
+import { cn } from "@/lib/utils";
+import { HomeIcon } from "lucide-react";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import {
+  FaGithub,
+  FaHome,
+  FaEnvelope,
+  FaLinkedin,
+  FaDownload,
+} from "react-icons/fa";
+import { motion } from "framer-motion";
 
 interface SidebarItemProps {
   section: string;
   heightAnchor: string;
+  sidebarState: "expanded" | "collapsed";
 }
 
 export default function SidebarItem({
   section,
   heightAnchor,
+  sidebarState,
 }: SidebarItemProps) {
   let displayedIcon;
+  const transition = {
+    duration: 0.8,
+    delay: 0.5,
+    ease: [0, 0.71, 0.2, 1.01],
+  };
 
   if (section === "Home") {
-    displayedIcon = Home_icon;
-  } else if (section === "GitHub") {
-    displayedIcon = github;
+    displayedIcon = <FaHome className="h-6 w-6 ml-1" />;
   } else if (section === "My Projects") {
-    displayedIcon = Download;
+    displayedIcon = <FaGithub className="h-6 w-6 ml-1" />;
   } else if (section === "Resume") {
-    displayedIcon = Download;
+    displayedIcon = <FaDownload className="h-6 w-6 ml-1" />;
   } else if (section === "Contact Me") {
-    displayedIcon = email;
+    displayedIcon = <FaEnvelope className="h-6 w-6 ml-1" />;
   } else if (section === "LinkedIn") {
-    displayedIcon = LinkedIn;
+    displayedIcon = <FaLinkedin className="h-6 w-6 ml-1" />;
   } else {
-    displayedIcon = Download;
+    displayedIcon = <FaGithub className="h-6 w-6 ml-1" />;
   }
 
+
   return (
-    <div className="flex items-center w-full justify-start h-20 text-sm mr-2 ml-2 font-medium text-white hover:bg-[#533d33] hover:text-white rounded-md">
+    // It would be good to have a cn() so that we can add the hover effect
+    <div className="flex items-center w-full justify-start h-12 text-sm mr-2 ml-2 font-medium text-white hover:text-white rounded-md">
       <a href={heightAnchor}>
-        <div className="flex items-center justify-between ml-2">
-          <Image src={displayedIcon} alt="Download" className="w-12 h-12" />
-          <span className="ml-2 text-3xl font-medium text-white">
-            {section}
-          </span>
+        <div className="flex items-center justify-between">
+          {displayedIcon}
+          {sidebarState === "expanded" && (
+            <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 0.2, duration: 0.8, ease: [0, 0.71, 0.2, 1.01],}}>
+              <span className="ml-2 text-xl font-medium text-white">
+                {section}
+              </span>
+            </motion.div>
+          )}
         </div>
       </a>
     </div>
