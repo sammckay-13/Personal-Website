19195bf60cc0f59bea159d77c94ff0c70816a1af
Sam McKay <sammckay31@gmail.com>
2026-08-04 12:35:05 -0600

    Add new logo

--- mckaypable/app/page.tsx
+++ mckaypable/app/page.tsx
@@ -1,19 +1,28 @@
 import { AppSidebar } from "@/components/AppSidebar";
-import {
-  Sidebar,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import ContentSection from "@/components/ContentSection";
+import BrownMckaypableLogo from "@/assets/imgs/BrownMckaypableLogo.png";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <SidebarProvider defaultOpen={true} >
-        <AppSidebar />
-          <SidebarTrigger />
-          {/* Your app goes here */}
-      </SidebarProvider>
-    </div>
+    <SidebarProvider defaultOpen={false} className=" bg-[#6F5345]">
+      <AppSidebar />
+      <div>
+        <div className="flex flex-col flex-1 h-100 w-screen  bg-blue-100">
+          <main className="mt-7 w-full">
+                          <Image
+                src={BrownMckaypableLogo}
+                alt="Mckaypable Logo"
+                className="h-10 w-14 ml-2"
+              />
+            <h1 className="text-3xl font-bold text-black">Mckaypable</h1>
+            <p className="mt-24">Sam McKay</p>
+          </main>
+        </div>
+        <ContentSection />
+        {/* Your app goes here */}
+      </div>
+    </SidebarProvider>
   );
 }
