99d8b75a75da5ec893e6495521e0c67e0822b9ac
Sam McKay <sammckay31@gmail.com>
2026-08-04 12:36:24 -0600

    add content section and custom text

---
+++ mckaypable/components/ContentSection.tsx
@@ -0,0 +1,10 @@
+import CustomText from "./CustomText";
+export default function ContentSection() {
+  return (
+    <div className="flex w-full h-75 flex-col flex-1 bg-red-100">
+      <h1 className="text-3xl font-bold text-black mt-10 ml-6">fdasjl</h1>
+      <div className="rounded-md bg-[#6F5345] h-1 ml-4 w-72"></div>
+      <CustomText />
+    </div>
+  );
+}
