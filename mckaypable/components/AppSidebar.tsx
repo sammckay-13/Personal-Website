e4d675ab3e7a5c0688a66d27b5d22e65c9899ed6
Sam McKay <sammckay31@gmail.com>
2026-08-04 12:35:25 -0600

    add logo and make x close button inside of sidebar

--- mckaypable/components/AppSidebar.tsx
+++ mckaypable/components/AppSidebar.tsx
@@ -1,31 +1,73 @@
+"use client";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
   SidebarHeader,
+  SidebarTrigger,
+  useSidebar,
 } from "@/components/ui/sidebar";
 import SidebarItem from "./SidebarItem";
-import Home_icon from "@/assets/imgs/Home_Icon.png";
+import { Menu, X } from "lucide-react";
+import { Button } from "./ui/button";
+import CreamMckaypableLogo from "@/assets/imgs/CreamMckaypableLogo.png";
 import Image from "next/image";
 
 export function AppSidebar() {
+  const {
+    state,
+    open,
+    setOpen,
+    openMobile,
+    setOpenMobile,
+    isMobile,
+    toggleSidebar,
+  } = useSidebar();
   return (
-    <Sidebar className="flex flex-col bg-[#6F5345]">
-      <SidebarHeader className="flex items-center justify-between bg-[#6F5345] text-white">
-        Mckaypable
-      </SidebarHeader>
-      <SidebarContent className="flex bg-[#6F5345] w-full">
-        <SidebarGroup className="flex items-center w-full">
-            <SidebarItem section="Home" heightAnchor="" />
-          <SidebarItem section="GitHub" heightAnchor="" />
-          <SidebarItem section="My Projects" heightAnchor="" />
-          <SidebarItem section="Resume" heightAnchor="" />
-          <SidebarItem section="Contact Me" heightAnchor="" />
-        </SidebarGroup>
-        <SidebarGroup />
-      </SidebarContent>
-      <SidebarFooter />
-    </Sidebar>
+    <div>
+      <Sidebar collapsible="icon" className="flex flex-col h-full">
+        <SidebarHeader className="flex items-center justify-between bg-[#6F5345] text-white">
+          {state !== "collapsed" && (
+            <div className="flex items-center justify-between w-full mt-2">
+              <Image
+                src={CreamMckaypableLogo}
+                alt="Mckaypable Logo"
+                className="h-10 w-14 ml-2"
+              />
+              <span className="text-xl font-bold">Mckaypable</span>
+              <Button variant="ghost" size="lg" onClick={toggleSidebar}>
+                <X className="h-10 w-10" />
+              </Button>
+            </div>
+          )}
+        </SidebarHeader>
+        <SidebarContent className="flex bg-[#6F5345] w-full h-full">
+          <SidebarGroup className="flex items-center w-full">
+            {state === "collapsed" && (
+              <Menu onClick={toggleSidebar} className="h-7 w-7 text-white" />
+            )}
+            <SidebarItem section="Home" heightAnchor="" sidebarState={state} />
+            <SidebarItem
+              section="My Projects"
+              heightAnchor=""
+              sidebarState={state}
+            />
+            <SidebarItem
+              section="Resume"
+              heightAnchor=""
+              sidebarState={state}
+            />
+            <SidebarItem
+              section="Contact Me"
+              heightAnchor=""
+              sidebarState={state}
+            />
+          </SidebarGroup>
+          <SidebarGroup />
+        </SidebarContent>
+        <SidebarFooter />
+      </Sidebar>
+    </div>
   );
 }
