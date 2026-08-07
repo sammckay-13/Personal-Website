689caeea089e70aff12388405c2f02ba534dd555
Sam McKay <sammckay31@gmail.com>
2026-08-07 09:09:24 -0600

    After reviewing this it's probably unnecessary but I'm keeping it in just in case I want to pass something unique into a content section

--- mckaypable/components/ContentSection.tsx
+++ mckaypable/components/ContentSection.tsx
@@ -1,10 +1,15 @@
 import CustomText from "./CustomText";
-export default function ContentSection() {
+
+interface ContentSectionProps {
+  children?: React.ReactNode;
+}
+
+export default function ContentSection({ children }: ContentSectionProps) {
   return (
-    <div className="flex w-full h-75 flex-col flex-1 bg-red-100">
+    <div className="flex h-75 flex-col flex-1 bg-red-100">
       <h1 className="text-3xl font-bold text-black mt-10 ml-6">fdasjl</h1>
-      <div className="rounded-md bg-[#6F5345] h-1 ml-4 w-72"></div>
       <CustomText />
+      {children}
     </div>
   );
 }
