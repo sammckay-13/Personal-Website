aca2020f914594e2e9b92b4f6aa883beffa0b7c1
Sam McKay <sammckay31@gmail.com>
2026-08-07 09:22:50 -0600

    change scroll bar color. Change this color later and find something better

--- mckaypable/app/layout.tsx
+++ mckaypable/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth scrollbar-track-sky-100 scrollbar-thumb-[#DC9954]`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
