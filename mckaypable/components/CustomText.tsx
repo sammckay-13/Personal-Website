28f0665fcd38cabbedc2c0245c684fdec7025b42
Sam McKay <sammckay31@gmail.com>
2026-08-10 16:47:20 -0600

    increase width so it takes up the right amount of the page

--- mckaypable/components/CustomText.tsx
+++ mckaypable/components/CustomText.tsx
@@ -1,6 +1,6 @@
 export default function CustomText() {
   return (
-    <div className="flex flex-col h-full ml-15 mt-10">
+    <div className="flex flex-col h-full ml-15 mt-10 w-10/12">
       <p className="text-2xl font-bold text-[#6F5345]">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget nisl
         euismod, ultrices augue eu, tincidunt nunc. Sed efficitur, nisl eu
