5a536151a70b843767e4731b14c5688dc0ad7619
Sam McKay <sammckay31@gmail.com>
2026-08-09 22:30:37 -0600

    hahaha it looks terrible on mobile

--- mckaypable/components/app-sidebar.tsx
+++ mckaypable/components/app-sidebar.tsx
@@ -3,9 +3,6 @@
 import * as React from "react";
 
 import { NavMain } from "@/components/nav-main";
-import { NavProjects } from "@/components/nav-projects";
-import { NavUser } from "@/components/nav-user";
-import { TeamSwitcher } from "@/components/team-switcher";
 import {
   Sidebar,
   SidebarContent,
