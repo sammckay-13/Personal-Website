99564aea9211a1e7cbd6681edde2e0d83fc6689c
Sam McKay <sammckay31@gmail.com>
2026-08-11 16:17:07 -0600

    use framer motion to animate the transition for the mckaypable sidebar group label, also conditionally pass an icon into the sidebar so my logo is visible even when collapsed

--- mckaypable/components/app-sidebar.tsx
+++ mckaypable/components/app-sidebar.tsx
@@ -8,14 +8,17 @@ import {
   SidebarContent,
   SidebarGroupLabel,
   SidebarHeader,
+  useSidebar,
 } from "@/components/ui/sidebar";
 import { Settings2Icon } from "lucide-react";
 import { FaGithub, FaHome } from "react-icons/fa";
 import Image from "next/image";
 import CreamMckaypableLogo from "@/assets/imgs/CreamMckaypableIcon.svg";
+import { motion } from "framer-motion";
+import { MckaypableLogo } from "@/assets/MckaypableLogo";
 
 // This is sample data.
-const data = {
+const dataWithoutLogo = {
   navMain: [
     {
       title: "Home",
@@ -29,18 +32,61 @@ const data = {
       url: "#",
       icon: <FaGithub className="text-[#FFFBEE]" />,
       items: [
-        {
-          title: "Genesis",
-          url: "#",
-        },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
+        // {
+        //   title: "Genesis",
+        //   url: "#",
+        // },
+        // {
+        //   title: "Explorer",
+        //   url: "#",
+        // },
+        // {
+        //   title: "Quantum",
+        //   url: "#",
+        // },
+      ],
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: <Settings2Icon className="text-[#FFFBEE]" />,
+      items: [],
+    },
+  ],
+};
+
+const dataWithLogo = {
+  navMain: [
+    {
+      title:"",
+      url: "#",
+      icon: <MckaypableLogo />,
+      items: [],
+    },
+    {
+      title: "Home",
+      url: "#",
+      icon: <FaHome className="text-[#FFFBEE]" />,
+      items: [],
+      isActive: true,
+    },
+    {
+      title: "My Projects",
+      url: "#",
+      icon: <FaGithub className="text-[#FFFBEE]" />,
+      items: [
+        // {
+        //   title: "Genesis",
+        //   url: "#",
+        // },
+        // {
+        //   title: "Explorer",
+        //   url: "#",
+        // },
+        // {
+        //   title: "Quantum",
+        //   url: "#",
+        // },
       ],
     },
     {
@@ -53,6 +99,7 @@ const data = {
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const { state } = useSidebar();
   return (
     <Sidebar
       collapsible="icon"
@@ -60,21 +107,30 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       className="bg-[#6F5345] border-transparent!"
     >
       {/* We'll see if I like there being no border on the sidebar */}
+
       <SidebarHeader className="bg-[#6F5345]">
         {" "}
-        <SidebarGroupLabel className="flex items-center justify-center gap-2 mt-3">
-          <Image
-            src={CreamMckaypableLogo}
-            alt="Mckaypable Logo"
-            className="h-12 w-12 mb-1"
-            loading="eager"
-          />
-          <span className="text-2xl text-[#FFFBEE]">Mckaypable</span>
-          {/* I don't like how the icon and text float away so I might change that later */}
-        </SidebarGroupLabel>
+        {state === "expanded" && (
+        <motion.div
+          initial={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.2, delay: 0.2 }}
+          animate={state === "expanded" ? { opacity: 1, y: 0 } : { opacity: 0, y: -10 }}
+        >
+          <SidebarGroupLabel className="flex items-center justify-center gap-2 mt-2">
+            <Image
+              src={CreamMckaypableLogo}
+              alt="Mckaypable Logo"
+              className="h-12 w-12 mb-1"
+              loading="eager"
+            />
+            <span className="text-2xl text-[#FFFBEE]">Mckaypable</span>
+            {/* I don't like how the icon and text float away so I might change that later */}
+          </SidebarGroupLabel>
+        </motion.div>
+        )}
       </SidebarHeader>
       <SidebarContent className="bg-[#6F5345] w-full">
-        <NavMain items={data.navMain} />
+        <NavMain items={state === "expanded" ? dataWithoutLogo.navMain : dataWithLogo.navMain} />
       </SidebarContent>
     </Sidebar>
   );
