4f980562ea623c9e264b78eababfddc94a140c15
Sam McKay <sammckay31@gmail.com>
2026-08-11 16:15:33 -0600

    Work on a direct svg version of my logo instead of an interpreted image. Also adjust animations and width for animated lines

---
+++ mckaypable/assets/MckaypableLogo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const MckaypableLogo = () => {
+    return (
+        <svg>
+            <path d="M 10,10 Q 10,10 10,10 T 4000,10" fill="transparent" stroke="#CE7052" strokeWidth="20" strokeLinecap="round" />
+        </svg>
+    )
+}
