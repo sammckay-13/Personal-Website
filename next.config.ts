2fd11358d7d10e56274b8c3c03c31ff265d0e95a
Sam McKay <sammckay31@gmail.com>
2026-08-12 13:33:46 -0600

    generate index.html

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {/* config options here */};
+const nextConfig: NextConfig = {  output: 'export',
+  basePath: '/personal-website',
+  images: {
+    unoptimized: true,
+  },};
 
 export default nextConfig;
