c11ea3eeecde60ce2864081aa6f5e9e3ac60abdd
Sam McKay <sammckay31@gmail.com>
2026-08-11 20:07:44 -0600

    Wow I haven't done linear algebra in a very long time so this took longer than I'd like to admit

--- mckaypable/assets/MckaypableLogo.tsx
+++ mckaypable/assets/MckaypableLogo.tsx
@@ -1,9 +1,50 @@
 import React from "react";
 
-export const MckaypableLogo = () => {
-    return (
-        <svg>
-            <path d="M 10,10 Q 10,10 10,10 T 4000,10" fill="transparent" stroke="#CE7052" strokeWidth="20" strokeLinecap="round" />
-        </svg>
-    )
-}
+export const MckaypableLogo = ({ color }: { color: string }) => {
+  return (
+    <svg viewBox="0 0 100% 100%" width="100%" height="100%" className="ml-0.75">
+      <path
+        d="M 1.95,9.75 L 9.75, 1.95 L 17.55,9.75"
+        fill="transparent"
+        stroke={color}
+        strokeWidth="2"
+        strokeLinecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M 9.75,9.75 L 17.55, 1.95 L 25.35,9.75"
+        fill="transparent"
+        stroke={color ?? "#FFFBEE"}
+        strokeWidth="2"
+        strokeLinecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M 1.95,9.75 L 25.35, 9.75"
+        fill="transparent"
+        stroke={color ?? "#FFFBEE"}
+        strokeWidth="2"
+        strokeLinecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M 1.95, 9.75 L 9.75,17.55 L 17.55,9.75"
+        fill="transparent"
+        stroke={color ?? "#FFFBEE"}
+        strokeWidth="2"
+        strokeLinecap="round"
+        opacity="0.5"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M 9.75, 9.75 L 17.55,17.55 L 25.35,9.75"
+        fill="transparent"
+        stroke={color ?? "#FFFBEE"}
+        strokeWidth="2"
+        strokeLinecap="round"
+        opacity="0.5"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
