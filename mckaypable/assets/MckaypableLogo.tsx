5dae892b054b196a758c476fe0e6da21d750d7fe
Sam McKay <sammckay31@gmail.com>
2026-08-12 12:33:59 -0600

    make the lines thicker

--- mckaypable/assets/MckaypableLogo.tsx
+++ mckaypable/assets/MckaypableLogo.tsx
@@ -5,7 +5,7 @@ export const MckaypableLogo = ({ color }: { color: string }) => {
         d="M 3.9,19.5 L 19.5, 3.9 L 35.1,19.5"
         fill="transparent"
         stroke={color}
-        strokeWidth="2"
+        strokeWidth="3"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
@@ -13,7 +13,7 @@ export const MckaypableLogo = ({ color }: { color: string }) => {
         d="M 19.5,19.5 L 35.1, 3.9 L 50.7,19.5"
         fill="transparent"
         stroke={color ?? "#FFFBEE"}
-        strokeWidth="2"
+        strokeWidth="3"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
@@ -21,7 +21,7 @@ export const MckaypableLogo = ({ color }: { color: string }) => {
         d="M 3.9,19.5 L 50.7, 19.5"
         fill="transparent"
         stroke={color ?? "#FFFBEE"}
-        strokeWidth="2"
+        strokeWidth="3"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
@@ -29,7 +29,7 @@ export const MckaypableLogo = ({ color }: { color: string }) => {
         d="M 3.5, 19.5 L 19.5,35.1 L 35.1,19.5"
         fill="transparent"
         stroke={color ?? "#FFFBEE"}
-        strokeWidth="2"
+        strokeWidth="3"
         strokeLinecap="round"
         opacity="0.5"
         strokeLinejoin="round"
@@ -38,7 +38,7 @@ export const MckaypableLogo = ({ color }: { color: string }) => {
         d="M 19.5, 19.5 L 35.1,35.1 L 50.7,19.5"
         fill="transparent"
         stroke={color ?? "#FFFBEE"}
-        strokeWidth="2"
+        strokeWidth="3"
         strokeLinecap="round"
         opacity="0.5"
         strokeLinejoin="round"
