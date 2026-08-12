0f37129e3f75c9b4589ee182005eb00fd2f69234
Sam McKay <sammckay31@gmail.com>
2026-08-11 20:08:20 -0600

    I was trying to pass the mckaypable logo as a button but that was more trouble than it was worth

--- mckaypable/components/app-sidebar.tsx
+++ mckaypable/components/app-sidebar.tsx
@@ -15,54 +15,10 @@ import { FaGithub, FaHome } from "react-icons/fa";
 import Image from "next/image";
 import CreamMckaypableLogo from "@/assets/imgs/CreamMckaypableIcon.svg";
 import { motion } from "framer-motion";
-import { MckaypableLogo } from "@/assets/MckaypableLogo";
 
 // This is sample data.
-const dataWithoutLogo = {
+const data = {
   navMain: [
-    {
-      title: "Home",
-      url: "#",
-      icon: <FaHome className="text-[#FFFBEE]" />,
-      items: [],
-      isActive: true,
-    },
-    {
-      title: "My Projects",
-      url: "#",
-      icon: <FaGithub className="text-[#FFFBEE]" />,
-      items: [
-        // {
-        //   title: "Genesis",
-        //   url: "#",
-        // },
-        // {
-        //   title: "Explorer",
-        //   url: "#",
-        // },
-        // {
-        //   title: "Quantum",
-        //   url: "#",
-        // },
-      ],
-    },
-    {
-      title: "Settings",
-      url: "#",
-      icon: <Settings2Icon className="text-[#FFFBEE]" />,
-      items: [],
-    },
-  ],
-};
-
-const dataWithLogo = {
-  navMain: [
-    {
-      title:"",
-      url: "#",
-      icon: <MckaypableLogo />,
-      items: [],
-    },
     {
       title: "Home",
       url: "#",
@@ -111,26 +67,30 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarHeader className="bg-[#6F5345]">
         {" "}
         {state === "expanded" && (
-        <motion.div
-          initial={{ opacity: 0, y: -10 }}
-          transition={{ duration: 0.2, delay: 0.2 }}
-          animate={state === "expanded" ? { opacity: 1, y: 0 } : { opacity: 0, y: -10 }}
-        >
-          <SidebarGroupLabel className="flex items-center justify-center gap-2 mt-2">
-            <Image
-              src={CreamMckaypableLogo}
-              alt="Mckaypable Logo"
-              className="h-12 w-12 mb-1"
-              loading="eager"
-            />
-            <span className="text-2xl text-[#FFFBEE]">Mckaypable</span>
-            {/* I don't like how the icon and text float away so I might change that later */}
-          </SidebarGroupLabel>
-        </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.2, delay: 0.2 }}
+            animate={
+              state === "expanded"
+                ? { opacity: 1, y: 0 }
+                : { opacity: 0, y: -10 }
+            }
+          >
+            <SidebarGroupLabel className="flex items-center justify-center gap-2 mt-2">
+              <Image
+                src={CreamMckaypableLogo}
+                alt="Mckaypable Logo"
+                className="h-12 w-12 mb-1"
+                loading="eager"
+              />
+              <span className="text-2xl text-[#FFFBEE]">Mckaypable</span>
+              {/* I don't like how the icon and text float away so I might change that later */}
+            </SidebarGroupLabel>
+          </motion.div>
         )}
       </SidebarHeader>
       <SidebarContent className="bg-[#6F5345] w-full">
-        <NavMain items={state === "expanded" ? dataWithoutLogo.navMain : dataWithLogo.navMain} />
+        <NavMain items={data.navMain} />
       </SidebarContent>
     </Sidebar>
   );
