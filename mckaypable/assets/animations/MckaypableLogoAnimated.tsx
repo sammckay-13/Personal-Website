cf11a5e6191dc79e7153dfb37e3999aac570aea0
Sam McKay <sammckay31@gmail.com>
2026-08-12 11:19:49 -0600

    make it smaller

--- mckaypable/assets/animations/MckaypableLogoAnimated.tsx
+++ mckaypable/assets/animations/MckaypableLogoAnimated.tsx
@@ -1,9 +1,8 @@
-
 export const MckaypableLogoAnimated = ({ color }: { color: string }) => {
   return (
     <svg viewBox="0 0 100% 100%" width="100%" height="100%" className="ml-0.75">
       <path
-        d="M 7.8,39 L 39, 7.8 L 70.2,39"
+        d="M 6.24,31.2 L 31.2, 6.24 L 56.16,31.2"
         fill="transparent"
         stroke={color}
         strokeWidth="4"
@@ -11,7 +10,7 @@ export const MckaypableLogoAnimated = ({ color }: { color: string }) => {
         strokeLinejoin="round"
       />
       <path
-        d="M 39,39 L 70.2, 7.8 L 101.4,39"
+        d="M 31.2,31.2 L 56.16, 6.24 L 81.12,31.2"
         fill="transparent"
         stroke={color ?? "#FFFBEE"}
         strokeWidth="4"
@@ -19,7 +18,7 @@ export const MckaypableLogoAnimated = ({ color }: { color: string }) => {
         strokeLinejoin="round"
       />
       <path
-        d="M 7.8,39 L 101.4, 39"
+        d="M 6.24,31.2 L 81.12, 31.2"
         fill="transparent"
         stroke={color ?? "#FFFBEE"}
         strokeWidth="4"
@@ -27,7 +26,7 @@ export const MckaypableLogoAnimated = ({ color }: { color: string }) => {
         strokeLinejoin="round"
       />
       <path
-        d="M 7, 39 L 39,70.2 L 70.2,39"
+        d="M 5.6, 31.2 L 31.2,56.16 L 56.16,31.2"
         fill="transparent"
         stroke={color ?? "#FFFBEE"}
         strokeWidth="4"
@@ -36,7 +35,7 @@ export const MckaypableLogoAnimated = ({ color }: { color: string }) => {
         strokeLinejoin="round"
       />
       <path
-        d="M 39, 39 L 70.2,70.2 L 101.4,39"
+        d="M 31.2, 31.2 L 56.12,56.16 L 81.12,31.2"
         fill="transparent"
         stroke={color ?? "#FFFBEE"}
         strokeWidth="4"
