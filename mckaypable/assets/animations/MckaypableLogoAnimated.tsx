b94a6d18991770eae7b4c430ad0c76597434c3fa
Sam McKay <sammckay31@gmail.com>
2026-08-12 09:30:38 -0600

    I'm being more particular about using svg paths instead of images because they are wayyyyy more versatile

--- mckaypable/assets/animations/MckaypableLogoAnimated.tsx
+++ mckaypable/assets/animations/MckaypableLogoAnimated.tsx
@@ -1,45 +1,45 @@
 
-export const MckaypableLogo = ({ color }: { color: string }) => {
+export const MckaypableLogoAnimated = ({ color }: { color: string }) => {
   return (
     <svg viewBox="0 0 100% 100%" width="100%" height="100%" className="ml-0.75">
       <path
-        d="M 3.9,19.5 L 19.5, 3.9 L 35.1,19.5"
+        d="M 7.8,39 L 39, 7.8 L 70.2,39"
         fill="transparent"
         stroke={color}
-        strokeWidth="2"
+        strokeWidth="4"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
-        d="M 19.5,19.5 L 35.1, 3.9 L 50.7,19.5"
+        d="M 39,39 L 70.2, 7.8 L 101.4,39"
         fill="transparent"
         stroke={color ?? "#FFFBEE"}
-        strokeWidth="2"
+        strokeWidth="4"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
-        d="M 3.9,19.5 L 50.7, 19.5"
+        d="M 7.8,39 L 101.4, 39"
         fill="transparent"
         stroke={color ?? "#FFFBEE"}
-        strokeWidth="2"
+        strokeWidth="4"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
-        d="M 3.5, 19.5 L 19.5,35.1 L 35.1,19.5"
+        d="M 7, 39 L 39,70.2 L 70.2,39"
         fill="transparent"
         stroke={color ?? "#FFFBEE"}
-        strokeWidth="2"
+        strokeWidth="4"
         strokeLinecap="round"
         opacity="0.5"
         strokeLinejoin="round"
       />
       <path
-        d="M 19.5, 19.5 L 35.1,35.1 L 50.7,19.5"
+        d="M 39, 39 L 70.2,70.2 L 101.4,39"
         fill="transparent"
         stroke={color ?? "#FFFBEE"}
-        strokeWidth="2"
+        strokeWidth="4"
         strokeLinecap="round"
         opacity="0.5"
         strokeLinejoin="round"
