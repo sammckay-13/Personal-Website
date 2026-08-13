484f62a7b382e9fe059a0dce13736b793f78d298
Sam McKay <sammckay31@gmail.com>
2026-08-12 20:05:10 -0600

    see if I could get the hydration issue to go away if I gave it constant projects instead of an iterative array

--- components/ProjectsWrapper.tsx
+++ components/ProjectsWrapper.tsx
@@ -10,18 +10,46 @@ import {
 import { Card, CardContent, CardHeader } from "./ui/card";
 import { LuExternalLink } from "react-icons/lu";
 
+interface Project {
+  name: string;
+  description: string;
+  url: string;
+  image: string | null;
+}
+
 export default function ProjectsWrapper() {
+  const myProjects: Project[] = [
+    {
+      name: "Genesis",
+      description: "A decentralized social media platform",
+      url: "https://genesis.social",
+      image: null,
+    },
+    {
+      name: "Explorer",
+      description: "A decentralized social media platform",
+      url: "https://explorer.social",
+      image: null,
+    },
+    {
+      name: "Quantum",
+      description: "A decentralized social media platform",
+      url: "https://quantum.social",
+      image: null,
+    },
+
+  ]
   return (
     <Carousel className="w-200 md:w-full">
       <CarouselContent className="ml-1">
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="basis-full pl-1 lg:basis-1/3">
+        {myProjects.map((project) => (
+          <CarouselItem key={project.name} className="basis-full pl-1 lg:basis-1/3">
             <div className="p-1">
               <Card className="flex flex-col md:w-full w-[30%]">
                 <CardHeader>
                   <div className="flex flex-row items-center justify-center">
-                    <h2 className="text-2xl font-bold text-black">fda</h2>{" "}
-                    <a href={"#"} target="_blank" rel="noreferrer">
+                    <h2 className="text-2xl font-bold text-black">project.name</h2>{" "}
+                    <a href={project.url} target="_blank" rel="noreferrer">
                       <LuExternalLink
                         size={20}
                         color={"#000"}
@@ -32,7 +60,7 @@ export default function ProjectsWrapper() {
                 </CardHeader>
                 <CardContent className="flex aspect-square items-center justify-center p-6">
                   <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <p>fda</p>
+                    <p>{project.description}</p>
                   </CardContent>
                 </CardContent>
               </Card>
