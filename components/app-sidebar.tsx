6ae136d9109aaca580b90c2b3a9b3541da339bab
Sam McKay <sammckay31@gmail.com>
2026-08-12 18:56:12 -0600

    I'm about to change a bunch of stuff to see if this works

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -54,26 +54,28 @@ const data = {
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  const { state } = useSidebar();
-  return (
+  const { state, isMobile } = useSidebar();
+  return ( 
     <Sidebar
-      collapsible="icon"
-      {...props}
-      className="bg-[#6F5345] border-transparent"
+    collapsible={isMobile ? 
+      "offcanvas" :
+      "icon"
+    }
+    {...props}
+    className="bg-[#6F5345] border-transparent"
     >
       {/* We'll see if I like there being no border on the sidebar */}
 
       <SidebarHeader className="bg-[#6F5345]">
-        {" "}
         {state === "expanded" && (
           <motion.div
-            initial={{ opacity: 0, y: -10 }}
-            transition={{ duration: 0.2, delay: 0.2 }}
-            animate={
-              state === "expanded"
-                ? { opacity: 1, y: 0 }
-                : { opacity: 0, y: -10 }
-            }
+          initial={{ opacity: 0, y: -10 }}
+          transition={{ duration: 0.2, delay: 0.2 }}
+          animate={
+            state === "expanded"
+            ? { opacity: 1, y: 0 }
+            : { opacity: 0, y: -10 }
+          }
           >
             <SidebarGroupLabel className="flex items-center justify-center gap-1 mt-2">
               <div className="flex w-15 h-10">
@@ -88,5 +90,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavMain items={data.navMain} />
       </SidebarContent>
     </Sidebar>
-  );
+  )
+
 }
