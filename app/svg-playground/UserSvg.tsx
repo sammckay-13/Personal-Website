9332ec514309917f75f9e56e492cd54631737cbb
Sam McKay <sammckay31@gmail.com>
2026-08-13 15:55:13 -0600

    add some components for svg input/color picker

---
+++ app/svg-playground/UserSvg.tsx
@@ -0,0 +1,43 @@
+export default function UserSvg({svgData}: {svgData: string}) {
+  return (
+    <svg viewBox="0 0 22 22" width="100%" height="100%" className="mr-1.5">
+      <path
+        d="M 1.95,9.75 L 9.75, 1.95 L 17.55,9.75"
+        fill="transparent"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M 9.75,9.75 L 17.55, 1.95 L 25.35,9.75"
+        fill="transparent"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M 1.95,9.75 L 25.35, 9.75"
+        fill="transparent"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M 1.95, 9.75 L 9.75,17.55 L 17.55,9.75"
+        fill="transparent"
+        strokeWidth="2"
+        strokeLinecap="round"
+        opacity="0.5"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M 9.75, 9.75 L 17.55,17.55 L 25.35,9.75"
+        fill="transparent"
+        strokeWidth="2"
+        strokeLinecap="round"
+        opacity="0.5"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
