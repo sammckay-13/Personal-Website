00aeb78fe85f419ff0e6d30b8656a07dc5b07516
Sam McKay <sammckay31@gmail.com>
2026-08-14 08:11:39 -0600

    prettier

--- assets/animations/MckaypableLogoAnimated.tsx
+++ assets/animations/MckaypableLogoAnimated.tsx
@@ -1,5 +1,5 @@
 "use client";
-import {motion} from "motion/react"
+import { motion } from "motion/react";
 export const MckaypableLogoAnimated = ({ color }: { color: string }) => {
   return (
     <svg viewBox="0 0 90 90" width="100%" height="100%" className="mr-3 mt-0.5">
@@ -10,9 +10,9 @@ export const MckaypableLogoAnimated = ({ color }: { color: string }) => {
         strokeWidth="4"
         strokeLinecap="round"
         strokeLinejoin="round"
-                          initial={{ pathLength: 0, opacity: 0 }} // Starts completely hidden
-          animate={{ pathLength: 1,  opacity:1 }} // Automatically draws the line
-          transition={{ duration: .75, ease: "easeOut", delay: 0.35, }}
+        initial={{ pathLength: 0, opacity: 0 }} // Starts completely hidden
+        animate={{ pathLength: 1, opacity: 1 }} // Automatically draws the line
+        transition={{ duration: 0.75, ease: "easeOut", delay: 0.35 }}
       />
       <motion.path
         d="M 25,25 L 45, 5 L 65,25"
@@ -21,9 +21,9 @@ export const MckaypableLogoAnimated = ({ color }: { color: string }) => {
         strokeWidth="4"
         strokeLinecap="round"
         strokeLinejoin="round"
-                          initial={{ pathLength: 0, opacity:0 }} // Starts completely hidden
-          animate={{ pathLength: 1,  opacity:1 }} // Automatically draws the line
-          transition={{ duration: .75, ease: "easeOut", delay: .35 }}
+        initial={{ pathLength: 0, opacity: 0 }} // Starts completely hidden
+        animate={{ pathLength: 1, opacity: 1 }} // Automatically draws the line
+        transition={{ duration: 0.75, ease: "easeOut", delay: 0.35 }}
       />
       <motion.path
         d="M 5,25 L 65,25"
@@ -32,9 +32,9 @@ export const MckaypableLogoAnimated = ({ color }: { color: string }) => {
         strokeWidth="4"
         strokeLinecap="round"
         strokeLinejoin="round"
-                          initial={{ pathLength: 0, opacity:0 }} // Starts completely hidden
-          animate={{ pathLength: 1,  opacity:1 }} // Automatically draws the line
-          transition={{ duration: .75, ease: "easeOut", delay: .35 }}
+        initial={{ pathLength: 0, opacity: 0 }} // Starts completely hidden
+        animate={{ pathLength: 1, opacity: 1 }} // Automatically draws the line
+        transition={{ duration: 0.75, ease: "easeOut", delay: 0.35 }}
       />
       <motion.path
         d="M 5, 25 L 25,45 L 45,25"
@@ -44,9 +44,9 @@ export const MckaypableLogoAnimated = ({ color }: { color: string }) => {
         strokeLinecap="round"
         opacity="0.5"
         strokeLinejoin="round"
-                          initial={{ pathLength: 0, opacity: 0 }} // Starts completely hidden
-          animate={{ pathLength: 1,  opacity:0.5 }} // Automatically draws the line
-          transition={{ duration: .75, ease: "easeOut", delay: .35 }}
+        initial={{ pathLength: 0, opacity: 0 }} // Starts completely hidden
+        animate={{ pathLength: 1, opacity: 0.5 }} // Automatically draws the line
+        transition={{ duration: 0.75, ease: "easeOut", delay: 0.35 }}
       />
       <motion.path
         d="M 25, 25 L 45,45.2 L 65,25"
@@ -56,9 +56,9 @@ export const MckaypableLogoAnimated = ({ color }: { color: string }) => {
         strokeLinecap="round"
         opacity="0.5"
         strokeLinejoin="round"
-                  initial={{ pathLength: 0, opacity: 0 }} // Starts completely hidden
-          animate={{ pathLength: 1,  opacity:0.5 }} // Automatically draws the line
-          transition={{ duration: .75, ease: "easeOut", delay: .35 }}
+        initial={{ pathLength: 0, opacity: 0 }} // Starts completely hidden
+        animate={{ pathLength: 1, opacity: 0.5 }} // Automatically draws the line
+        transition={{ duration: 0.75, ease: "easeOut", delay: 0.35 }}
       />
     </svg>
   );
